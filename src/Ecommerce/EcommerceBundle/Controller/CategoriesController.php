7d1fd189346ac8224811fb7149e1c1528ceef835
aziztdl <tdl.abdelaziz@gmail.com>
2019-07-02 12:55:37 +0200

    test

--- src/Ecommerce/EcommerceBundle/Controller/CategoriesController.php
+++ src/Ecommerce/EcommerceBundle/Controller/CategoriesController.php
@@ -9,6 +9,8 @@ class CategoriesController extends Controller
     public function menuAction()
     {
         $em = $this->getDoctrine()->getManager();
+
+        
         $categories = $em->getRepository('EcommerceBundle:Categories')->findAll();
         
         return $this->render('EcommerceBundle:Default:categories/modulesUsed/menu.html.twig', array('categories' => $categories));
